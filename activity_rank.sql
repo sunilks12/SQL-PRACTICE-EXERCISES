cf979f96c4a3dcacac01e4cab63eda66d89c137d
Sunil Kamkar Sheshagiri <41332941+sunilks12@users.noreply.github.com>
2023-01-04 23:29:13 -0600

    Create activity_rank.sql

---
+++ activity_rank.sql
@@ -0,0 +1,12 @@
+Find the email activity rank for each user. Email activity rank is defined by the total number of emails sent. The user with the highest number of emails sent will have a rank of 1, and so on. Output the user, total emails, and their activity rank. Order records by the total emails in descending order. Sort users with the same number of emails in alphabetical order.
+In your rankings, return a unique value (i.e., a unique rank) even if multiple users have the same number of emails.
+
+with users as(
+select from_user,count(id) as total_emails
+from google_gmail_emails group by from_user
+)
+select *,
+row_number() over (order by total_emails desc) row_number
+from users 
+order by total_emails desc,from_user asc
+
