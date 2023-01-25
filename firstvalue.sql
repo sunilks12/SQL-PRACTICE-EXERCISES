f2ff7ab23ec4b02eee167abb801250830366d325
Sunil Kamkar Sheshagiri <41332941+sunilks12@users.noreply.github.com>
2023-01-24 18:25:12 -0600

    Create firstvalue.sql

---
+++ firstvalue.sql
@@ -0,0 +1,4 @@
+SELECT order_id,order_date,customer_name,city, order_amount,
+FIRST_VALUE(order_date) OVER(PARTITION BY city ORDER BY city) first_order_date,
+LAST_VALUE(order_date) OVER(PARTITION BY city ORDER BY city) last_order_date
+FROM [dbo].[Orders]
